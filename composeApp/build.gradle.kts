cd755eea091afc989a7bd3ededcf77f304b81bee
Aaron J Yoder <aaronjyoder@gmail.com>
2025-12-31 23:28:12 -0500

    Update application build settings

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,76 +2,93 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
+	alias(libs.plugins.kotlinMultiplatform)
+	alias(libs.plugins.composeMultiplatform)
+	alias(libs.plugins.composeCompiler)
+	alias(libs.plugins.composeHotReload)
 }
 
 repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+	google()
+	mavenCentral()
+	maven { url = uri("https://jitpack.io") }
 }
 
 kotlin {
-    jvm()
-    
-    js {
-        browser()
-        binaries.executable()
-    }
-    
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser()
-        binaries.executable()
-    }
-    
-    sourceSets {
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
-        }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
-        }
-        jvmMain.dependencies {
-            implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutinesSwing)
+	jvm()
 
-            implementation("com.formdev:flatlaf:3.7")
-            implementation("com.formdev:flatlaf-intellij-themes:3.7")
+	js {
+		browser()
+		binaries.executable()
+	}
 
-            implementation("io.github.aaronjyoder:fill:1.0.0-0.2206")
-            implementation("com.github.aaronjyoder:polylabel-java-mirror:1.3.0")
+	@OptIn(ExperimentalWasmDsl::class) wasmJs {
+		browser()
+		binaries.executable()
+	}
 
-            implementation("com.riskrieg:map:1.0.0-2.2206")
-            implementation("com.riskrieg:palette:1.1.0-4.2206")
-            implementation("com.riskrieg:codec:1.0.0-3.2206")
+	sourceSets {
+		commonMain.dependencies {
+			implementation(compose.runtime)
+			implementation(compose.foundation)
+			implementation(compose.material3)
+			implementation(compose.ui)
+			implementation(compose.components.resources)
+			implementation(compose.components.uiToolingPreview)
+			implementation(libs.androidx.lifecycle.viewmodelCompose)
+			implementation(libs.androidx.lifecycle.runtimeCompose)
+		}
+		commonTest.dependencies {
+			implementation(libs.kotlin.test)
+		}
+		jvmMain.dependencies {
+			implementation(compose.desktop.currentOs)
+			implementation(libs.kotlinx.coroutinesSwing)
 
-            implementation("tools.jackson.module:jackson-module-kotlin:3.0.3") // Needed for workaround at EditorModel.kt#343
+			implementation("com.formdev:flatlaf:3.7")
+			implementation("com.formdev:flatlaf-intellij-themes:3.7")
 
-            implementation("org.jgrapht:jgrapht-io:1.5.2")
-        }
-    }
+			implementation("io.github.aaronjyoder:fill:1.0.0-0.2206")
+			implementation("com.github.aaronjyoder:polylabel-java-mirror:1.3.0")
+
+			implementation("com.riskrieg:map:1.0.0-2.2206")
+			implementation("com.riskrieg:palette:1.1.0-4.2206")
+			implementation("com.riskrieg:codec:1.0.0-3.2206")
+
+			implementation("tools.jackson.module:jackson-module-kotlin:3.0.3") // Needed for workaround at EditorModel.kt#343
+
+			implementation("org.jgrapht:jgrapht-io:1.5.2")
+		}
+	}
 }
 
+fun getProjectProperty(name: String) = project.properties[name] as? String
 
 compose.desktop {
-    application {
-        mainClass = "com.openkrieg.MainKt"
+	application {
+		mainClass = "com.openkrieg.MainKt"
 
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.openkrieg"
-            packageVersion = "1.0.0"
-        }
-    }
+		nativeDistributions {
+			targetFormats(TargetFormat.Msi, TargetFormat.Exe, TargetFormat.Deb, TargetFormat.Dmg)
+			packageName = "com.openkrieg"
+			packageVersion = "2.8.6"
+			description = "The official Openkrieg map editor."
+			vendor = "Openkrieg"
+
+			linux {
+				packageName = "openkrieg-map-editor"
+				debMaintainer = "Openkrieg <contact@adelheid.org>"
+				menuGroup = "Openkrieg"
+			}
+
+			windows {
+				menuGroup = "Openkrieg"
+				upgradeUuid = getProjectProperty("guid")
+			}
+
+			macOS {
+				bundleID = "com.openkrieg.mapeditor"
+			}
+		}
+	}
 }
